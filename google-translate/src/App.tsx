dc2f80d57425f66c9beb7860ea272bbd50fc2a05
JulianOviedo <julianovie234@gmail.com>
2023-04-26 12:30:12 -0300

    reducer

--- google-translate/src/App.tsx
+++ google-translate/src/App.tsx
@@ -1,7 +1,66 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { useReducer } from 'react'
+
+const initialState = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+function reducer (state: typeof initialState, action) {
+  const { type, payload } = action
+
+  if (type === 'INTERCHANGE_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      fromText: payload,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      result: payload
+    }
+  }
+
+  return state
+}
+
 function App () {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
     <>
-      <h1>Hello world</h1>
+      <h1>google translate</h1>
     </>
   )
 }
