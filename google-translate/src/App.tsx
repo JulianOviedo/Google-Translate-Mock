03ba3ef57be46bcdb8bbde0fe4621b2e24f3b2ed
JulianOviedo <julianovie234@gmail.com>
2023-04-26 12:47:19 -0300

    restructure

--- google-translate/src/App.tsx
+++ google-translate/src/App.tsx
@@ -1,64 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import { useReducer } from 'react'
-import { type Action } from './types'
-
-const initialState = {
-  fromLanguage: 'auto',
-  toLanguage: 'en',
-  fromText: '',
-  result: '',
-  loading: false
-}
-
-function reducer (state: typeof initialState, action: Action) {
-  const { type } = action
-
-  if (type === 'INTERCHANGE_LANGUAGES') {
-    return {
-      ...state,
-      fromLanguage: state.toLanguage,
-      toLanguage: state.fromLanguage
-    }
-  }
-
-  if (type === 'SET_FROM_LANGUAGE') {
-    return {
-      ...state,
-      fromLanguage: action.payload
-    }
-  }
-
-  if (type === 'SET_TO_LANGUAGE') {
-    return {
-      ...state,
-      toLanguage: action.payload
-    }
-  }
-
-  if (type === 'SET_FROM_TEXT') {
-    return {
-      ...state,
-      loading: true,
-      fromText: action.payload,
-      result: ''
-    }
-  }
-
-  if (type === 'SET_RESULT') {
-    return {
-      ...state,
-      loading: false,
-      result: action.payload
-    }
-  }
-
-  return state
-}
 
 function App () {
-  const [state, dispatch] = useReducer(reducer, initialState)
-
   return (
     <>
       <h1>google translate</h1>
---
+++ google-translate/src/hooks/useStore.ts
@@ -0,0 +1,70 @@
+import { type Action } from '../types'
+
+const initialState = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+function reducer (state: typeof initialState, action: Action) {
+  const { type } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      fromText: action.payload,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      result: action.payload
+    }
+  }
+
+  return state
+}
+
+export function useStore () {
+  const [{
+    fromLanguage,
+    toLanguage,
+    fromText, result, loading
+  }, dispatch] = useReducer(reducer, initialState)
+
+  return {
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading
+  }
+}
